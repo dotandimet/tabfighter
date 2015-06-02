39df0a327ee20c2eeabf6eb79b9724ac587a6762
Dotan Dimet <dotan@corky.net>
2015-06-02 19:17:48 +0300

    work in progress, transition from counts to data points

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var self = require("sdk/self");
 var windows = require("sdk/windows").browserWindows;
 var tabs = require("sdk/tabs");
 var moment = require("moment");
+var { setTimeout } = require("sdk/timers");
 
 var button = ToggleButton({
   id: "tabfighter-button",
@@ -14,6 +15,7 @@ var button = ToggleButton({
     "64": "./tabfighter2-64.png"
   },
   badge: 0,
+  badgeColor: '#0000ff',
   onClick: handleChange
 });
 
@@ -22,13 +24,16 @@ function countOpenTabs(){
 }
 
 var tabStats = {
- opened: 0,
- closed: 0,
- oldest: null,
- newest: null,
- lastClosed : null,
+ opened: [],
+ closed: [],
  all : {}
 };
+
+function oldestTab() {
+  for( let t of tabs ) {
+    getStatForTab(t.id).birth;
+  }
+}
 
 function getStatForTab(id) {
   if (!tabStats.all[id]) {
@@ -70,9 +75,10 @@ tabs.on('activate', tabActive);
 tabs.on('deactivate', tabInactive);
 
 tabs.on('open', function(tab){
-  tabStats.opened += 1;
+  tabStats.opened.push( getStatForTab(tab).birth );
   countOpenTabs();
-})
+});
+
 tabs.on('ready', tabReady);
 
 tabs.on('close', function(tab) {
@@ -92,27 +98,30 @@ function surveyOpenTabs(tabCollection) {
   }
   countOpenTabs();
 }
-
-surveyOpenTabs(tabs);
-
-var panel = panels.Panel({
-contentURL: self.data.url("panel.html"),
-contentScriptFile: self.data.url("stats.js"),
-onHide: handleHide
-});
-
+// run survey on startup:
+// maybe timer will make this happen after session restore?
+setTimeout( function() {
+    surveyOpenTabs(tabs);
+    panel = panels.Panel({
+      contentURL: self.data.url("panel.html"),
+      contentScriptFile: self.data.url("stats.js"),
+      onHide: handleHide,
+    });
 panel.on('show', function() {
   tabInactive(tabs.activeTab); // increment love
   let stats = Object.keys(tabStats.all).map(function(k) { return tabStats.all[k]; });
   
   stats.map(function(i) { i.AGE = moment.duration(i.birth - moment()).humanize(true); i.LOVE = (i.love > 0) ? moment.duration(i.love).humanize() : 'none'; });
-  panel.port.emit('stats', { "opened": tabStats.opened, "closed": tabStats.closed, "stats": stats } );
+  panel.port.emit('stats', { "opened": tabStats.opened, "closed": tabStats.closed, "stats": stats, "overflowed": checkInvisibleTabs() } );
 });
 
-panel.port.on('picktab', function(id) { for (t of tabs) { if (t.id === id) { t.activate(); } } });
+panel.port.on('picktab', function(id) { for (let t of tabs) { if (t.id === id) { t.activate(); } } });
 
+    }, 2000);
+
+var panel = null;
 function handleChange(state) {
-  if (state.checked) {
+  if (state.checked && panel !== null) {
     panel.show({
     position: button
   });
@@ -121,6 +130,23 @@ function handleChange(state) {
 
 function handleHide() {
   button.state('window', {checked: false});
+}
+
+
+//var { modelFor } = require("sdk/model/core");
+var { viewFor } = require("sdk/view/core");
+var tab_utils = require("sdk/tabs/utils");
+
+function getBrowser(tab) {
+  var llt = viewFor(tab);
+  var tabBrowser = tab_utils.getTabBrowserForTab(llt);
+  return tabBrowser;
+}
+
+function checkInvisibleTabs() {
+  var browse = getBrowser(tabs.activeTab);
+  console.log('browser tabs: ' + browse.tabs.length + ' visible: ' + browse.visibleTabs.length);
+  return (browse.tabs.length > browse.visibleTabs.length) ? (browse.tabs.length - browse.visibleTabs.length) : false;
 }
 
 
